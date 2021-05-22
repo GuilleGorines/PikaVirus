25499186ba180726213d0c5cc175a8ab608d4257
GuilleGorines <guillermo.gorines@gmail.com>
2021-05-22 14:41:28 +0200

    updated download script

--- bin/Download_all_assemblies.py
+++ bin/Download_all_assemblies.py
@@ -3,7 +3,8 @@ import urllib.request
 import argparse
 import os
 
-
+import random
+random.seed(5)
 ############################################
 ####### 1. Parameters for the script #######
 ############################################
@@ -12,16 +13,18 @@ parser = argparse.ArgumentParser(description="Download assemblies from the NCBI 
 
 parser.add_argument("--only_ref_gen", type=bool, metavar="bool", default=False, dest="onlyref" , help="Download only those assemblies with the \"reference genome\" category (Default: False).")
 parser.add_argument("--strains", type=bool , default=False,metavar="bool", dest="strains", help="Look for strains in the input taxids.")
-parser.add_argument("-group", default="all", dest="group", choices=["virus","bacteria","fungi","all"], help="The group of assemblies from which download. Available: \"virus\", \"bacteria\", \"fungi\" (Default: all).")
-parser.add_argument("--only_sheet", type= bool,metavar="bool", default = False, dest="only_sheet", help="Download only the information sheet, in the proper order for pikavirus to work.")
-parser.add_argument("--only_complete_genome", type =bool, metavar="bool", default = False, dest="only_complete_genome", help="Download only those assemblies corresponding to complete genomes.")
+parser.add_argument("-group", default="all", dest="group", choices=["virus","bacteria","fungi","all"], help="The group of assemblies from which download. Available: \"all\", \"virus\", \"bacteria\", \"fungi\" (Default: all).")
+parser.add_argument("--only_sheet", type= bool,metavar="bool", default = False, dest="only_sheet", help="Download only the information sheet, in the proper order for pikavirus to work (Default: False).")
+parser.add_argument("--only_complete_genome", type =bool, metavar="bool", default = False, dest="only_complete_genome", help="Download only those assemblies corresponding to complete genomes (Default: False).")
+parser.add_argument("single_assembly_per_species_taxid", type="bool", metavar="bool", default = False, dest="single_assembly_per_spp_taxid", help="Dowload only one assembly for each species taxid, reference-genome and complete genomes if able (Default: False).")
+parser.add_argument("single_assembly_per_subspecies_taxid", type="bool", metavar="bool", default = False, dest="single_assembly_per_strain_taxid", help="Dowload only one assembly for each subspecies/strain taxid, reference-genome and complete genomes if able (Default: False).")
 args = parser.parse_args()
 
 #########################################################
 ####### 2. Get the refseq assembly data from NCBI #######
 #########################################################
 
-# 2.1: change virus for viral (thats how it figures in refseq)
+# change virus for viral (thats how it figures in refseq)
 
 if args.group.lower() == "all":
     groups_to_download = ["viral","bacteria","fungi"]
@@ -35,9 +38,12 @@ if args.group.lower() == "bacteria":
 if args.group.lower() == "virus":
     groups_to_download = ["viral"]
 
-# 2.2: access the assembly file
+# for each group
 
 for group in groups_to_download:
+
+    # access the assembly file from ncbi
+
     assembly_url = f"ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/{group}/assembly_summary.txt"
 
     with urllib.request.urlopen(assembly_url) as response:
@@ -47,24 +53,82 @@ for group in groups_to_download:
   
     filtered_assembly_data = [line for line in assembly_data if len(line)==22]
 
-# if activated, choose only complete genomes references
+    # if activated, choose only complete genomes references
     if args.only_complete_genome == True:
         filtered_assembly_data = [line for line in filtered_assembly_data if line[11]=="Complete Genome"]
 
-# if activated, choose only reference genomes
+    # if activated, choose only reference genomes
     if args.onlyref == True:
         filtered_assembly_data = [line for line in filtered_assembly_data if line[4]=="reference genome"]
 
-# 3.3: remove repeated lines (avoid repeating downloads) and incomplete assemblies
+    # if activated, choose only one assembly per taxid (reference genomes and complete genomes are priority)
+    if args.single_assembly_per_spp_taxid == True or args.single_assembly_per_strain_taxid == True:
+        
+        assembly_per_species_taxid = {}
+        
+        # one dict entry for each taxid, containing all assemblies refering to it
+        # keys can be spp taxid or strain/subspp taxid
+
+        for line in filtered_assembly_data:
+            if args.single_assembly_per_spp_taxid == True:
+                if line[5] not in assembly_per_species_taxid.keys():
+                    assembly_per_species_taxid[line[5]] = [line]
+                else:
+                    assembly_per_species_taxid[line[5]].append(line)
+
+            elif args.single_assembly_per_strain_taxid == True:
+                if args.single_assembly_per_spp_taxid == True and args.single_assembly_per_strain_taxid == True:
+                    print(f"If single_assembly_per_species_taxid and single_assembly_per_subspecies_taxid are both set to True, only subspecies/strains assemblies will be retrieved.")
+                
+                if line[6] not in assembly_per_species_taxid.keys():
+                    assembly_per_species_taxid[line[6]] = [line]
+                else:
+                    assembly_per_species_taxid[line[6]].append(line)
+
+        filtered_assembly_data = []
+
+        # give a score to each assembly
+        for _, data in assembly_per_species_taxid.items():
+            
+            chosen_list = []
+            max_score = 0
+
+            for datapiece in data:
+                score = 0
+
+                if datapiece[4] == "reference genome":
+                    score += 6
+                if datapiece[4] == "representative genome":
+                    score += 4
+
+                if datapiece[11] == "Complete genome":
+                    score += 5
+                if datapiece[11] == "Chromosome":
+                    score += 3
+
+                if datapiece[13] == "Full":
+                    score += 1
+
+                if score > max_score:
+                    max_score = score
+
+                chosen_list.append(datapiece.append(score))
+                
+            chosen_list = [item[:-1] for item in chosen_list if item[-1] == max_score]
+            chosen_assembly = random.choice(chosen_list)
+            filtered_assembly_data.append(chosen_assembly)
+
+
+    # remove repeated lines (avoid repeating downloads) and incomplete assemblies (should not happen at this point)
     filtered_assembly_data = [list(y) for y in set([tuple(x) for x in filtered_assembly_data])]
     assembly_quantity = len(filtered_assembly_data)
     
-# 3.4: obtain the relevant information: 
-# assembly accession(0), subspecies taxid(1), species taxid(2), organism name(3), infraespecific name(4), assembly_level(5), ftp path(6)
+    # obtain the relevant information: 
+    # assembly accession(0), subspecies taxid(1), species taxid(2), organism name(3), infraespecific name(4), assembly_level(5), ftp path(6)
     filtered_assembly_data = [[col[0],col[5],col[6],col[7],col[8],col[11],col[19]] for col in filtered_assembly_data]
 
     #############################################################################
-    ####### 4. Generate the urls with an informative tsv for traceability #######
+    ####### 3. Generate the urls with an informative tsv for traceability #######
     #############################################################################
 
     for single_assembly in filtered_assembly_data:
@@ -74,20 +138,20 @@ for group in groups_to_download:
         download_url = f"{ftp_path}/{file_url}_genomic.fna.gz"
         single_assembly.append(download_url)
 
-# generate the name for the file for download
+    # generate the name for the file for download
 
         filename = f"{single_assembly[0]}.fna.gz"
         single_assembly.append(filename)
 
-# assembly accession(0)
-# subspecies taxid(1)
-# species taxid(2)
-# organism name(3)
-# infraespecific name(4)
-# assembly_level(5)
-# ftp path(6)
-# download_url(7)
-# filename(8)
+    # assembly accession(0)
+    # subspecies taxid(1)
+    # species taxid(2)
+    # organism name(3)
+    # infraespecific name(4)
+    # assembly_level(5)
+    # ftp path(6)
+    # download_url(7)
+    # filename(8)
 
     tsv_file = f"{group}_assemblies.tsv"
 
@@ -97,32 +161,34 @@ for group in groups_to_download:
         for col in filtered_assembly_data:
             outfile.write(f"{col[0]}\t{col[2]}\t{col[1]}\t{col[3]}\t{col[4]}\t{col[7]}\t{col[8]}\t{col[5]}\n")
 
-print(f"{tsv_file} created successfully!")
+    print(f"{tsv_file} created successfully!")
 
-if args.only_sheet == False:
-    ##############################################
-    ####### 5. Download the assembly files #######
-    ##############################################
-    print(f"Starting download of {assembly_quantity} {group} assemblies!")
-    destiny_folder = f"{group}_assemblies_for_pikavirus"
-    
-    if not os.path.exists(destiny_folder):
-        os.mkdir(destiny_folder)
+    if args.only_sheet == False:
 
-    for col in filtered_assembly_data:
-        try:
-            url = col[7]
-            filename = col[8]
-            location_filename = f"{destiny_folder}/{filename}"
-            if os.path.exists(location_filename):
-                print(f"{filename} already found on destiny file.")
-            else:
-                urllib.request.urlretrieve(url, location_filename)               
+        ##############################################
+        ####### 4. Download the assembly files #######
+        ##############################################
 
-        except:
-            print(f"Assembly {col[0]} from organism {col[3]} could not be retrieved.")
+        print(f"Starting download of {assembly_quantity} {group} assemblies!")
+        destiny_folder = f"{group}_assemblies_for_pikavirus"
+        
+        if not os.path.exists(destiny_folder):
+            os.mkdir(destiny_folder)
 
-    print(f"Download of {group} assemblies complete!")
+        for col in filtered_assembly_data:
+            try:
+                url = col[7]
+                filename = col[8]
+                location_filename = f"{destiny_folder}/{filename}"
+                if os.path.exists(location_filename):
+                    print(f"{filename} already found on destiny file.")
+                else:
+                    urllib.request.urlretrieve(url, location_filename)               
+
+            except:
+                print(f"Assembly {col[0]} from organism {col[3]} could not be retrieved.")
+
+        print(f"Download of {group} assemblies complete!")
 
 print("All done!")
 sys.exit(0)
@@ -133,7 +199,7 @@ sys.exit(0)
 # Column 3: "wgs_master"
 # Column 4: "refseq_category"
 # Column 5: "taxid"
-# Column 6: "species_taxid"
+# Column 6: "(sub)species_taxid"
 # Column 7: "organism_name"
 # Column 8: "infraspecific_name"
 # Column 9: "isolate"
