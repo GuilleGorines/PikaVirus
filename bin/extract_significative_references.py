c8e77dca689fe3edc53df79f71c5843d6a414fef
GuilleGorines <guillermo.gorines@gmail.com>
2021-07-23 12:50:49 +0200

    changed threshold to improve sensiblity

--- bin/extract_significative_references.py
+++ bin/extract_significative_references.py
@@ -77,7 +77,7 @@ with open(mashresult) as infile:
             numerator, denominator = line[1].split("/")
             shared = int(numerator)/int(denominator)
 
-            if shared > 0.5:
+            if shared > 0.1:
                 chosen.append(line[4].split("/")[-1])
     
 # files
