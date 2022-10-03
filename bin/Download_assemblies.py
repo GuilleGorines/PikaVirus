ad589268582e01d8f5ba25373ea7a3ac847561af
GuilleGorines <guillermo.gorines@gmail.com>
2022-10-03 17:19:55 +0200

    Update Download_assemblies.py

--- bin/Download_assemblies.py
+++ bin/Download_assemblies.py
@@ -9,11 +9,12 @@ AUTHOR: Guillermo J. Gorines Cordero
 
 MAIL: guillermo.gorines@urjc.es
 
-VERSION: 1.0
+VERSION: 1.1
 
 CREATED: Exact date unknown (late 2020)
 
-REVISED: 26-5-2021
+REVISED: 26-05-2021
+         03-10-2022
 
 DESCRIPTION: 
     Downloads the refseq assemblies needed by nf-core pikavirus (.fna.gz format),
@@ -30,8 +31,11 @@ OPTIONS:
 USAGE:
     Download_all_assemblies.py 
         [-group]
-        [--only_ref_gen]
+        [-database]
         [--only_sheet]
+        
+        **SOON**
+        [--only_ref_gen]
         [--only_complete_genome]
         [--single_assembly_per_species_taxid]
         [--single_assembly_per_subspecies_taxid]
@@ -54,23 +58,152 @@ import sys
 # Specialiced imports
 import urllib.request
 
+# Variables for colors
+info    = "\033[0;34m" + "INFO" + "\033[0m"
+error   = "\033[0;31m" + "ERROR" + "\033[0m"
+warning = "\033[1;33m" + "WARNING" + "\033[0m"
+success = "\033[0;32m" + "SUCCESS" + "\033[0m"
 
-def download(url):
-    try:
-        urllib.request.urlretrieve(url, location_filename)               
-    except:
+
+# Local functions
+def Get_db_assembly_list(database, group):
+    """
+    Get the assembly list 
+    """
+    reference = f"ftp://ftp.ncbi.nlm.nih.gov/genomes/{database}/{group}/assembly_summary.txt"
+
+    with urllib.request.urlopen(reference) as response:
+        assembly_list = response.read().decode("utf-8").split("\n")
+
+    assembly_data = [line.split("\t") for line in assembly_list if not line.startswith("#")]
+    filtered_assembly_data = [line for line in assembly_data if len(line) == 23]
+
+    """
+    Anchor code, just in case the options "only complete genome" and "only ref" are accepted
+
+    # if activated, choose only complete genomes references
+    if args.only_complete_genome == True:
+        filtered_assembly_data_refseq = [line for line in filtered_assembly_data_refseq if line[11]=="Complete Genome"]
+
+    # if activated, choose only reference genomes
+    if args.onlyref == True:
+        filtered_assembly_data_refseq = [line for line in filtered_assembly_data_refseq if line[4]=="reference genome"]
+    """
+    filtered_assembly_data = [list(y) for y in set([tuple(x) for x in filtered_assembly_data])]
+    assembly_quantity = len(filtered_assembly_data)
+    
+    # Obtain the relevant information: 
+    # 0 - Assembly accession
+    # 1 - Subspecies taxid
+    # 2 - Species taxid
+    # 3 - Organism name (scientific name)
+    # 4 - Infraespecific name
+    # 5 - Assembly_level
+    # 6 - FTP path
+    # 7 - repeated seqs the other db
+
+    assemblies = [[col[0], col[5], col[6], col[7], col[8], col[11], col[19], col[17]] for col in filtered_assembly_data]
+
+    return assemblies, assembly_quantity
+
+def Join_refseq_gb(refseq_assembly, genbank_assembly):
+    """
+    Join the refseq and the genbank assembly files
+    """
+    redundant_assemblies = [col[7] for col in refseq_assembly]
+    filtered_data_genbank = [line for line in genbank_assembly if line[0] not in redundant_assemblies]
+
+    merged_assembly_data = refseq_assembly + filtered_data_genbank
+
+    non_redundant_genbank_number = len(filtered_data_genbank)
+    total_number = len(merged_assembly_data)
+
+    return merged_assembly_data, total_number, non_redundant_genbank_number
+
+def Write_assembly_data(assembly_data, group):
+    """
+
+    """
+    with open(f"{}_assemblies.tsv","w") as outfile:
+        outfile.write(f"# Assemblies chosen for the group {group} \n")
+        outfile.write(f"# Assembly_accession\tSpecies_taxid\tSubspecies_taxid\tScientific_name\tIntraespecific_name\tDownload_URL\tFile_name\tAssembly_level\n")
+        for col in assembly_data:
+            outfile.write(f"{col[0]}\t{col[2]}\t{col[1]}\t{col[3]}\t{col[4]}\t{col[6]}\t{col[7]}\t{col[5]}\n")
+
+    return
+
+def Download_assemblies(assembly_data, group):
+    """
+
+    """
+    # Check destiny folder
+    destiny_folder = f"{group}_assemblies_for_pikavirus"
+    if not os.path.exists(destiny_folder):
+        os.mkdir(destiny_folder)
+
+    for single_assembly in assembly_data:
+
+        # Name for the download
+        filename = f"{single_assembly[0]}.fna.gz" 
+        location_filename = f"{destiny_folder}/{filename}"
         
+        # URL name
+        file_url = single_assembly[6].split("/")[-1]
+        download_url = f"{ftp_path}/{file_url}_genomic.fna.gz"
+
+        # Counter to see how many were correctly downloaded
+        successful_downloads = 0
+        unsuccessful_downloads = 0
+
+        try:
+            if os.path.exists(location_filename):
+                print(f"{warning}: {filename} already found on destiny directory. Download skipped.")
+            else:
+                urllib.request.urlretrieve(url, location_filename)
+                successful_downloads += 1     
+        except:
+            print(f"{error}: Assembly {single_assembly[0]} from organsim {single_assembly[3]} could not be retrieved. URL: {download_url}")
+            unsuccessful_downloads += 1
+
+        print(f"{success}: Download of {group} assemblies complete!")
+        print(f"{info}: {successful_downloads} {group} assemblies downloaded successfully")
+        print(f"{info}: {unsuccessful_downloads} {group} assemblies could not be downloaded")
+
+    return 
 
 ############################################
 ####### 1. Parameters for the script #######
 ############################################
 
-parser = argparse.ArgumentParser(description="Download assemblies from the NCBI refseq, already named for their immediate use in nf-core pikavirus. An internet network is required for this script to work.")
+parser = argparse.ArgumentParser(
+    description="Download assemblies from the NCBI refseq, already named for their immediate use in nf-core pikavirus. An internet network is required for this script to work."
+    )
 
-parser.add_argument("-group", default="all", dest="group", choices=["virus","bacteria","fungi","all"], help="The group of assemblies from which download. Available: \"all\", \"virus\", \"bacteria\", \"fungi\" (Default: all).")
-parser.add_argument("-database", default="all", dest="database", choices = ["refseq", "genbank", "all"], help="The database to download the assemblies from. Available: \"refseq\", \"genbank\",\"all\" (Default: all).")
+parser.add_argument(
+    "-group",
+    default="all", 
+    dest="group", 
+    choices=["virus","bacteria","fungi","all"],
+    help="The group of assemblies from which download. Available: \"all\", \"virus\", \"bacteria\", \"fungi\" (Default: all)."
+    )
+
+parser.add_argument(
+    "-database",
+    default="all",
+    dest="database",
+    choices = ["refseq", "genbank", "all"],
+    help="The database to download the assemblies from. Available: \"refseq\", \"genbank\",\"all\" (Default: all)."
+    )
+
+parser.add_argument(
+    "--only_sheet",
+    action='store_true',
+    default = False,
+    dest="only_sheet",
+    help="Download only the information sheet, in the proper order for pikavirus to work (Default: False)."
+    )
+    
 # parser.add_argument("--only_ref_gen", action='store_true', default=False, dest="onlyref" , help="Download only those assemblies with the \"reference genome\" category (Default: False).")
-parser.add_argument("--only_sheet", action='store_true', default = False, dest="only_sheet", help="Download only the information sheet, in the proper order for pikavirus to work (Default: False).")
 # parser.add_argument("--only_complete_genome", action='store_true', default = False, dest="only_complete_genome", help="Download only those assemblies corresponding to complete genomes (Default: False).")
 # parser.add_argument("--single_assembly_per_species_taxid", action='store_true', default = False, dest="single_assembly_per_spp_taxid", help="Dowload only one assembly for each species taxid, reference-genome and complete genomes if able (Default: False).")
 # parser.add_argument("--single_assembly_per_subspecies_taxid", action='store_true', default = False, dest="single_assembly_per_strain_taxid", help="Dowload only one assembly for each subspecies/strain taxid, reference-genome and complete genomes if able (Default: False).")
@@ -80,176 +213,51 @@ args = parser.parse_args()
 ####### 2. Get the refseq assembly data from NCBI #######
 #########################################################
 
-# change virus for viral (thats how it figures in refseq)
-
-if args.group.lower() == "all":
-    groups_to_download = ["viral","bacteria","fungi"]
-
-if args.group.lower() == "fungi":
-    groups_to_download = ["fungi"]
-
-if args.group.lower() == "bacteria":
-    groups_to_download = ["bacteria"]
-
-if args.group.lower() == "virus":
-    groups_to_download = ["viral"]
+groups_to_download = ["virus","bacteria","fungi"] if args.group.lower() == "all" else [args.group.lower()]
 
 # for each group
 
 for group in groups_to_download:
-
-    # access the assembly file from NCBI
-
-    if args.database == "refseq" or args.database == "all":
-
-        # generate url
-        assembly_url_refseq = f"ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/{group}/assembly_summary.txt"
-
-        print(assembly_url_refseq)
-
-        with urllib.request.urlopen(assembly_url_refseq) as response:
-            assembly_file_refseq = response.read().decode("utf-8").split("\n")
-
-        print(assembly_file_refseq)
-
-        assembly_data_refseq = [line.split("\t") for line in assembly_file_refseq if not line.startswith("#")]
-
-        filtered_assembly_data_refseq = [line for line in assembly_data_refseq if len(line) == 23]
-
-        # if activated, choose only complete genomes references
-        if args.only_complete_genome == True:
-            filtered_assembly_data_refseq = [line for line in filtered_assembly_data_refseq if line[11]=="Complete Genome"]
-
-        # if activated, choose only reference genomes
-        if args.onlyref == True:
-            filtered_assembly_data_refseq = [line for line in filtered_assembly_data_refseq if line[4]=="reference genome"]
-
-        # remove repeated lines (avoid repeating downloads) and incomplete assemblies (should not happen at this point)
-        filtered_assembly_data_refseq = [list(y) for y in set([tuple(x) for x in filtered_assembly_data_refseq])]
-        
-        assembly_refseq_quantity = len(filtered_assembly_data_refseq)
-        
-        # remove redundant entries between refseq and genbank
-        if args.database == "all":
-            genbank_in_refseq = [line[17] for line in filtered_assembly_data_refseq]
-
-        # obtain the relevant information: 
-        # assembly accession(0), subspecies taxid(1), species taxid(2), organism name(3), infraespecific name(4), assembly_level(5), ftp path(6)
-        assemblies_refseq = [[col[0],col[5],col[6],col[7],col[8],col[11],col[19]] for col in filtered_assembly_data_refseq]
-
-    # same process for genbank
-    elif args.database == "genbank" or args.database == "all":
-
-        assembly_url_genbank = f"ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/{group}/assembly_summary.txt"
-
-        with urllib.request.urlopen(assembly_url_genbank) as response:
-            assembly_file_genbank = response.read().decode("utf-8").split("\n")
-        
-
-        print(assembly_url_genbank)
-        print(assembly_file_genbank)
-
-        assembly_data_genbank = [line.split("\t") for line in assembly_file_genbank if not line.startswith("#")]
-        filtered_assembly_data_genbank = [line for line in assembly_data_genbank if len(line)==23]
-
-        if args.only_complete_genome == True:
-            filtered_assembly_data_genbank = [line for line in filtered_assembly_data_genbank if line[11]=="Complete Genome"]
-        # if activated, choose only reference genomes
-        if args.onlyref == True:
-            filtered_assembly_data_genbank = [line for line in filtered_assembly_data_genbank if line[4]=="reference genome"]
-        if args.database == "all":
-            filtered_assembly_data_genbank = [line for line in filtered_assembly_data_genbank if line[0] not in genbank_in_refseq]
-        
-        filtered_assembly_data_genbank = [list(y) for y in set([tuple(x) for x in filtered_assembly_data_genbank])]
-
-        assembly_genbank_quantity = len(filtered_assembly_data_genbank)
-        
-        assemblies_genbank = [[col[0],col[5],col[6],col[7],col[8],col[11],col[19]] for col in filtered_assembly_data_genbank]
-        
+    if group == "virus":
+        group == "viral"
+    
     if args.database == "all":
-        final_assemblies = assemblies_refseq + assemblies_genbank
-    elif args.database == "genbank":
-        final_assemblies = assemblies_genbank
-    elif args.database == "refseq":
-        final_assemblies = assemblies_refseq
+        assembly_list_refseq, refseq_number = Get_db_assembly_list("refseq", group)
+        assembly_list_genbank, genbank_number = Get_db_assembly_list("genbank", group)
+        merged_assembly_list, total_number, non_redundant_genbank_number  = Join_refseq_gb(assembly_list_refseq, assembly_list_genbank)
+        Write_assembly_data(merged_assembly_list, group)
 
-    #############################################################################
-    ####### 3. Generate the urls with an informative tsv for traceability #######
-    #############################################################################
+        print(f"{info}: {refseq_number} total RefSeq assemblies were found")
+        print(f"{info}: {genbank_number} total GenBank assemblies were found")
 
-    for single_assembly in final_assemblies:
-        # generate the url for download
-        ftp_path = single_assembly[-1]
-        file_url = ftp_path.split("/")[-1]
-        download_url = f"{ftp_path}/{file_url}_genomic.fna.gz"
-        single_assembly.append(download_url)
+        if args.only_sheet:
+            print(f"{info}: {total_number} assemblies were included in the assembly sheet: ({refseq_number} from RefSeq; {non_redundant_genbank_number} from GenBank)")
+            print(f"{success}: All done!")
+            sys.exit(0)
 
-    # generate the name for the file for download
+        print(f"{info}: {total_number} assemblies will be downloaded ({refseq_number} RefSeq; {non_redundant_genbank_number} GenBank)")
+        Download_assemblies(merged_assembly_list, group)
 
-        filename = f"{single_assembly[0]}.fna.gz"
-        single_assembly.append(filename)
+    else:
+        assembly_list, number = Get_db_assembly_list(args.database, group)
+        Write_assembly_data(assembly_list, group)
 
-    # assembly accession(0)
-    # subspecies taxid(1)
-    # species taxid(2)
-    # organism name(3)
-    # infraespecific name(4)
-    # assembly_level(5)
-    # ftp path(6)
-    # download_url(7)
-    # filename(8)
+        nice_name = "RefSeq" if args.database == "refseq" else "GenBank"
 
-    tsv_file = f"{group}_assemblies.tsv"
+        print(f"{info}: {number} total assemblies were found in {nice_name}")
 
-    with open(tsv_file,"w") as outfile:
-        outfile.write(f"# Assemblies chosen for the group {group} \n")
-        outfile.write(f"# Assembly_accession\tSpecies_taxid\tSubspecies_taxid\tScientific_name\tIntraespecific_name\tDownload_URL\tFile_name\tAssembly_level\n")
-        for col in final_assemblies:
-            outfile.write(f"{col[0]}\t{col[2]}\t{col[1]}\t{col[3]}\t{col[4]}\t{col[7]}\t{col[8]}\t{col[5]}\n")
+        if args.only_sheet:
+            print("All done!")
+            sys.exit(0)
 
-    print(f"{tsv_file} created successfully!")
+        print(f"{info}: {number} {nice_name} assemblies will be downloaded")
+        Download_assemblies(merged_assembly_list, group)
+    
+    print(f"{success}: All done!")
+    sys.exit(0)
 
-    if args.only_sheet == False:
 
-        ##############################################
-        ####### 4. Download the assembly files #######
-        ##############################################
-        if args.database == "all":
 
-            total_assemblies = assembly_refseq_quantity + assembly_genbank_quantity
-
-            print(f"Starting download of {total_assemblies} {group} assemblies!\n \
-                    {assembly_refseq_quantity} assemblies from RefSeq\n \
-                    {assembly_genbank_quantity} assemblies from GenBank")
-
-        elif args.database == "refseq":
-             print(f"Starting download of {assembly_refseq_quantity} {group} assemblies from RefSeq!")
-            
-        elif args.database == "genbank":
-             print(f"Starting download of {assembly_genbank_quantity} {group} assemblies from GenBank!")
-
-        destiny_folder = f"{group}_assemblies_for_pikavirus"
-        
-        if not os.path.exists(destiny_folder):
-            os.mkdir(destiny_folder)
-
-        for col in final_assemblies:
-            try:
-                url = col[7]
-                filename = col[8]
-                location_filename = f"{destiny_folder}/{filename}"
-                if os.path.exists(location_filename):
-                    print(f"{filename} already found on destiny file.")
-                else:
-                    urllib.request.urlretrieve(url, location_filename)               
-
-            except:
-                print(f"Assembly {col[0]} from organism {col[3]} could not be retrieved. URL: {url}")
-
-        print(f"Download of {group} assemblies complete!")
-
-print("All done!")
-sys.exit(0)
 
 # Column 0: "assembly_accession"
 # Column 1: "bioproject"
